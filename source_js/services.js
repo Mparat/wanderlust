a59ab6bb7b8e164d99c7b9a9c419be29b15cdfe7
Meera Parat <meera.parat@gmail.com>
2016-11-29 21:37:23 -0600

    cleaning

--- source_js/services.js
+++ source_js/services.js
@@ -1,12 +1,8 @@
 var myApp = angular.module('myApp', []);
 
 myApp.factory('CommonData', function($http, $window){
-<<<<<<< HEAD
   var selected = {};    // Shared data between MainController and MapController
   // Passes selected images between the gallery and map views.
-=======
-  var selected = {};
->>>>>>> 0d1058509ef2a126f6167c122bb24297163c3a85
   return{
       getSelected : function(){
         return selected;
